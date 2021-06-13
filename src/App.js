0e681a967951bcd2d153f41bdadfd6273cae8ed0
BeliMoreta <77322967+BeliMoreta@users.noreply.github.com>
2021-06-13 21:44:18 +0200

    Logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './ReaderLogo.svg';
+import logo from './Libro.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"  />
+        <img src={logo} className="App-logo" alt="logo" />
 
 Dictionary React 
       </header>
