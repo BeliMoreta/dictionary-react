7ec68bb344efdf5a2c179c9ed9990a6d6fc13f18
BeliMoreta <77322967+BeliMoreta@users.noreply.github.com>
2021-06-13 19:55:57 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './ReaderLogo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"  />
+
+Dictionary React 
+      </header>
+    </div>
+  );
+}
+
+export default App;
