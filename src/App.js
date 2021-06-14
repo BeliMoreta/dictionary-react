84ecef76a131a557cbbbc21b567659dc6b26261c
BeliMoreta <77322967+BeliMoreta@users.noreply.github.com>
2021-06-14 21:51:00 +0200

    input

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import logo from './Libro.png';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
+
+     
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
+        <br></br>
 Dictionary React 
       </header>
+
+<main>
+<div>
+<Dictionary />
+</div>
+</main>
+
+      <footer> 
+        
+      <span className="copyright">© Coded by BeliMoreta</span>
+          <br></br>
+        <a href="https://github.com/BeliMoreta/my-weather-react">GitHub</a>
+        <br></br>
+        <a href="https://www.linkedin.com/in/isabel-aguado-moreta-101a69126/">Linkedin</a>
+
+      </footer> 
+
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+function Search(event) {
+    event.preventDefault()
+alert("Searching");
+}
+
+
+return (
+<div className="Dictionary">
+    <form onSubmit={Search}>  
+        <imput  type="search" autoFocus={true} />  
+     </form>
+</div>
+);
+}
+  
