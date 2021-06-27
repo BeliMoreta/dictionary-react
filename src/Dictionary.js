380b125217cf933e930c818e115bfccf76ba778b
BeliMoreta <77322967+BeliMoreta@users.noreply.github.com>
2021-06-27 20:14:04 +0200

    photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,32 @@ import React, {useState} from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
     let [Keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
     function handleResponse(response) {
         setResults(response.data[0])
     }
 
+    function handlePexelsResponse(response) {
+        setPhotos(response.data.photos);
+      }
+
     function Search() {
         //documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${Keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey = "563492ad6f91700001000001f17f4b49f761425fab2dde8a91b24470";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${Keyword}&per_page=10`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
+
     }
 
     function handleSubmit(event) {
@@ -50,6 +62,7 @@ return (
      </div>
      </section>
      <Results results={results}/>
+     <Photos photos={photos} />
 </div>
 );
 } else {
